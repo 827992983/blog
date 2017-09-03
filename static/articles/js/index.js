6827d540303779ffc2f8da977ad2fa57b07317ee
abellee <abellee@yunify.com>
2017-09-03 16:18:05 +0800

    fix search chinese string

--- static/articles/js/index.js
+++ static/articles/js/index.js
@@ -1,9 +1,31 @@
 window.onload = admin_onload;
 
 function admin_onload() {
-
     document.getElementById('prevPage').onclick = prevPage;
     document.getElementById('nextPage').onclick = nextPage;
+}
+
+function getParam(decode_url, name) {
+    try{
+        var params = decode_url.split("?");
+        params = params[1].split("&");
+        for(var i=0; i<params.length; i++) {
+            var param = params[i];
+            kv = param.split('=');
+            if(kv[0] == name){
+                var index = kv[1].indexOf('#');
+                if(index > 0){
+                    var keywords = kv[1].substring(0, index);
+                    return keywords;
+                }else{
+                    return kv[1];
+                }
+            }
+        }
+    }catch(err){
+        return null;
+    }
+    return null;
 }
 
 function prevPage() {
@@ -19,11 +41,13 @@ function prevPage() {
     if (page_index < 1){
         page_index = 1;
     }
-    var uri = baseUri + '?page_index=' + String(page_index);
+    var uri = baseUri + '?page_index=' + page_index;
 
-    var keywords = getUrlPara('keywords');
+    var decode_url = decodeURIComponent(window.location);
+    var keywords = getParam(decode_url, 'keywords');
+
     if(keywords != null){
-        uri = uri + '&keywords=' + String(keywords);
+        uri = uri + '&keywords=' + keywords;
     }
 
     window.location.href = uri;
@@ -41,11 +65,14 @@ function nextPage() {
     if (page_index > window.g_total){
         page_index = window.g_total;
     }
-    var uri = baseUri + '?page_index=' + String(page_index);
+    var uri = baseUri + '?page_index=' + page_index;
 
-    var keywords = getUrlPara('keywords');
+
+    var decode_url = decodeURIComponent(window.location);
+    var keywords = getParam(decode_url, 'keywords');
+
     if(keywords != null){
-        uri = uri + '&keywords=' + String(keywords);
+        uri = uri + '&keywords=' + keywords;
     }
 
     window.location.href = uri;
